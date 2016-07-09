f6e1a89ca28fcaf464cc92f23cb9d997ff27f0a6
diegaco <diegoacosta211@gmail.com>
2016-07-09 20:37:11 -0300

    critical Css

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,57 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'css/style.css': 'css/main.scss'
-                }
-            }
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
         },
-        watch: {
-            options: {
-                livereload: true,
-            },
-            html: {
-                files: 'index.html'
-            },
-            css: {
-                files: 'css/*.scss',
-                tasks: 'sass'
-            }
-        },
-        express: {
-            server: {
-                options: {
-                    bases: './',
-                    port: '9090',
-                    open: true,
-                    livereload: true
-                }
-            } 
+        files: {
+          'css/style.css': 'css/main.scss'
         }
-  });  
-  
+      }
+    },
+    watch: {
+      options: {
+        livereload: true,
+      },
+      html: {
+        files: 'index.html'
+      },
+      css: {
+        files: 'css/*.scss',
+        tasks: 'sass'
+      }
+    },
+    express: {
+      server: {
+        options: {
+          bases: './',
+          port: '9090',
+          open: true,
+          livereload: true
+        }
+      }
+    },
+    critical: {
+      test: {
+        options: {
+          base: './',
+          css: [
+            'labs/performanceTest/css/style.css'
+          ],
+          width: 1300,
+          height: 900
+        },
+        src: 'labs/performanceTest/index.html',
+        dest: 'labs/performanceTest/index-critical.html'
+      }
+    }
+
+  });
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-express');
+  grunt.loadNpmTasks('grunt-critical');
 
   grunt.registerTask('start', ['express','watch']);
-
 };
