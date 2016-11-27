38ee8e09415c37cf22d3a1484364c0e37cca513d
diegaco <diegoacosta211@gmail.com>
2016-11-26 21:19:15 -0300

    internal pages optimization

--- lab/frontend-nanodegree-mobile-portfolio/gulpfile.js
+++ lab/frontend-nanodegree-mobile-portfolio/gulpfile.js
@@ -4,8 +4,8 @@ var gulp = require('gulp');
 var critical = require('critical');
 
 
-// Generate & Inline Critical-path CSS
-gulp.task('critical', function (cb) {
+// Generate & Inline Critical-path CSS for Index.html
+gulp.task('critical-home', function (cb) {
   critical.generate({
     inline: true,
     base: './',
@@ -14,6 +14,49 @@ gulp.task('critical', function (cb) {
     minify: true,
     src: 'index.html',
     dest: 'index-critical.html',
+    css: 'css/style.css'
+  });
+});
+
+
+// Generate & Inline Critical-path CSS for project-2048.html
+gulp.task('critical-project2048', function (cb) {
+  critical.generate({
+    inline: true,
+    base: './',
+    width: 1440,
+    height: 900,
+    minify: true,
+    src: 'project-2048.html',
+    dest: 'project-2048-critical.html',
+    css: 'css/style.css'
+  });
+});
+
+// Generate & Inline Critical-path CSS for project-mobile.html
+gulp.task('critical-mobile', function (cb) {
+  critical.generate({
+    inline: true,
+    base: './',
+    width: 1440,
+    height: 900,
+    minify: true,
+    src: 'project-mobile.html',
+    dest: 'project-mobile-critical.html',
+    css: 'css/style.css'
+  });
+});
+
+// Generate & Inline Critical-path CSS for project-webperf.html
+gulp.task('critical-webperf', function (cb) {
+  critical.generate({
+    inline: true,
+    base: './',
+    width: 1440,
+    height: 900,
+    minify: true,
+    src: 'project-webperf.html',
+    dest: 'project-webperf-critical.html',
     css: 'css/style.css'
   });
 });
